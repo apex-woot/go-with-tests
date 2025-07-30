efda8bf4dcbf8e8d3023f780f90e9b26a26972cd
Vitalii Kohut <vkohut@intelliarts.com>
2025-07-30 20:29:56 +0300

    feat: Test sum of empty slices

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -6,3 +6,23 @@ func Sum(arr []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	sums := make([]int, 0, len(numbersToSum))
+	for _, s := range numbersToSum {
+		sums = append(sums, Sum(s))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	sums := make([]int, len(numbersToSum))
+	for i, s := range numbersToSum {
+		tail := make([]int, 0)
+		if len(s) > 0 {
+			tail = s[1:]
+		}
+		sums[i] = Sum(tail)
+	}
+	return sums
+}
